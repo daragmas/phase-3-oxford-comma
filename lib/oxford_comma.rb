fac1e588baf66da7458bdfc474816b2daeeae52a
daragmas <daragmas@gmail.com>
2022-08-08 17:52:21 -0400

    commit tree 85d989d776409e14183499485001d105efed802b

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.length == 2
+        array[2] = array[1]
+        array[1] = 'and'
+        return array.join(' ')
+    elsif array.length < 2
+        return array.join
+    else
+        array[-1] = "and #{array[-1]}"
+        array.join(", ")
+    end 
 end
